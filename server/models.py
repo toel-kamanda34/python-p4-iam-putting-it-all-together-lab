69ff4e9dfa5dcf0eb2749e2465688230e2ccf754
toel-kamanda34 <toelokemwa@gmail.com>
2024-10-17 02:36:52 +0300

    Models passing the test

--- server/models.py
+++ server/models.py
@@ -7,9 +7,62 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key =True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+    
+    recipes = db.relationship('Recipe', back_populates='user')
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+    
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username must be present")
+        return username
+
+    def __repr__(self):
+        return f'<User {self.username}, ID: {self.id}>'
+
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.Relationship('User', back_populates='recipes')
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError(" Title must be present")
+        return title
+    
+    @validates('instructions')
+    def validate_instructions(self, key, instructions):
+        if not instructions or len(instructions) < 50:
+            raise ValueError("Instrucrions must be atleat 50 characters long")
+        return instructions 
+
+    def __repr__(self):
+        return f'<Recipe {self.id}: {self.title}>'
